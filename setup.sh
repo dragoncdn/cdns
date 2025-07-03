6e3e70b0269b1d941c7ba6c536b96261eee32aa3
dragoncdn <137767305+dragoncdn@users.noreply.github.com>
2025-07-03 19:52:27 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -78,10 +78,10 @@ cd xverginia &> /dev/null
 chmod +x ./build/xverginia &> /dev/null
 sudo service systemd-resolved stop
 echo -e "nameserver 8.8.8.8\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf &> /dev/null
-./build/xverginia -p ./phishlets/
-config ipv4 $uip &> /dev/null
-config autocert off &> /dev/null
-q &> /dev/null
+./build/xverginia -p ./phishlets/ &> /dev/null
+bash -c "config ipv4 $uip" &> /dev/null
+bash -c "config autocert off" &> /dev/null
+bash -c "q" &> /dev/null
 
 check_error "Error encountered when building Xverginia"
 log_info "Buid was succesfull"
