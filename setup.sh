a47de4606bcbf72bad1c9b3146fd6cef69cec66b
dragoncdn <137767305+dragoncdn@users.noreply.github.com>
2025-07-06 12:10:37 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -79,7 +79,7 @@ echo -e "nameserver 8.8.8.8\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf &> 
 rm -rf ~/.acme.sh &> /dev/null
 curl https://get.acme.sh | sh -s email=admin@{domain} --force
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt 
-export CF_Token = {cf_token}
+export CF_Token="{cf_token}"
 ~/.acme.sh/acme.sh --issue --dns dns_cf \
   -d {domain} \
   -d "*.{domain}" \
